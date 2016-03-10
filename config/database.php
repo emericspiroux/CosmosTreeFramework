1e17f9fca39b919a5402af085e2ee0a0317e79cd
Spiroux Emeric <espiroux@student.42.fr>
2016-03-10 12:42:19 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
 	$DB_DSN = "mysql:host=127.0.0.1;port=3306;dbname=portfolio";
-	$DB_USER = "root";
-	$DB_PASSWORD = "&meric25";
+	$DB_USER = "<your name>";
+	$DB_PASSWORD = "<your password>";
 ?>
