8022a1ffb4ad191346d966006a338a8e07e201c0
espiroux <emeric.spiroux@gmail.com>
2016-04-03 17:09:07 +0200

    add mail

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -9,7 +9,7 @@ $(function() {
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
-            
+
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://192.168.1.29:8080/CosmosTreeFramework/mother/contact",
                 type: "POST",
                 data: {
                     name: name,
@@ -30,7 +30,7 @@ $(function() {
                     message: message
                 },
                 cache: false,
-                success: function() {
+                success: function(data) {
                     // Enable button & show success message
                     $("#btnSubmit").attr("disabled", false);
                     $('#success').html("<div class='alert alert-success'>");
--- controller/mother.php
+++ controller/mother.php
@@ -16,4 +16,16 @@ class mother extends CF_controller
 		header("Content-Disposition: inline;filename=cvEmericSpiroux2016.pdf");
 		echo file_get_contents(__BASE_URL__."assets/cvEmericSpiroux2016.pdf");
 	}
+
+	public function contact(){
+		header("Access-Control-Allow-Origin: *");
+		$name = $this->post->getValidClean('name', "nom", "required");
+		$email = $this->post->getValidClean('email', "email", "required");
+		$phone = $this->post->getValidClean('phone', "telephone", "required");
+		$message = $this->post->getValidClean('message', "message", "required");
+
+		if ($this->post->check()){
+			$this->mail->sendContact($name, $email, $phone, $message);
+		}
+	}
 }
